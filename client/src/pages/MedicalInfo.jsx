2c2e6655da29fc03086e8900325ae77c30287513
indhu <indhu00712@gmail.com>
2023-12-11 11:03:34 +0530

    Register form

---
+++ client/src/pages/MedicalInfo.jsx
@@ -0,0 +1,146 @@
+import React, { useState } from 'react';
+
+function MedicalInfo({formData,setFormData}) {
+  
+
+  const handleInputChange = (e, category, index) => {
+    const { name, value } = e.target;
+    const updatedConditions = [...formData[category]];
+    updatedConditions[index][name] = value;
+
+    setFormData({
+      ...formData,
+      [category]: updatedConditions,
+    });
+  };
+
+  const handleAddEntry = (category) => {
+    setFormData({
+      ...formData,
+      [category]: [...formData[category], {}],
+    });
+  };
+
+  
+
+  return (
+    <div className='medical-info-container'>
+      
+      <form >
+        <div>
+          <p>
+            If you have/had any medical conditions for a prolonged time and took up treatment, please mention below:
+          </p>
+          {formData.prolongedMedicalConditions.map((condition, index) => (
+            <div key={index}>
+              <label className='form-label'>
+                Condition Name:
+                <input className='form-input'
+                  type="text"
+                  name="conditionName" placeholder='Enter Condition Name'
+                  value={condition.conditionName || ''}
+                  onChange={(e) => handleInputChange(e, 'prolongedMedicalConditions', index)}
+                />
+              </label>
+              <br />
+              <label className='form-label'>
+                Diagnosis Date:
+                <input className='form-input'
+                  type="date"
+                  name="diagnosisDate"
+                  value={condition.diagnosisDate || ''}
+                  onChange={(e) => handleInputChange(e, 'prolongedMedicalConditions', index)}
+                />
+              </label>
+              <br />
+              <label className='form-label'>
+                Treatment:
+                <input className='form-input'
+                  type="text"
+                  name="treatment" placeholder='Enter the treatment taken up'
+                  value={condition.treatment || ''}
+                  onChange={(e) => handleInputChange(e, 'prolongedMedicalConditions', index)}
+                />
+              </label>
+              <br />
+            </div>
+          ))}
+          <button type="button" onClick={() => handleAddEntry('prolongedMedicalConditions')}>
+            Add Condition
+          </button>
+        </div>
+
+        <div>
+          <p>If you have allergies, choose them and choose their severity:</p>
+          {formData.allergies.map((allergy, index) => (
+            <div key={index}>
+              <label className='form-label'>
+                Allergy Name:
+                <input className='form-input'
+                  type="text"
+                  name="allergyName" placeholder='Enter Allergy Name'
+                  value={allergy.allergyName || ''}
+                  onChange={(e) => handleInputChange(e, 'allergies', index)}
+                />
+              </label>
+              <br />
+              <label className='form-label'>
+                Severity:
+                <select className='form-input'
+                  name="severity"
+                  value={allergy.severity || ''}
+                  onChange={(e) => handleInputChange(e, 'allergies', index)}
+                >
+                  <option value="">Select Severity</option>
+                  <option value="Mild">Mild</option>
+                  <option value="Moderate">Moderate</option>
+                  <option value="Severe">Severe</option>
+                </select>
+              </label>
+              <br />
+            </div>
+          ))}
+          <button type="button" onClick={() => handleAddEntry('allergies')}>
+            Add Allergy
+          </button>
+        </div>
+
+        <div>
+          <p>If you have undergone surgeries in the past, enter the details below:</p>
+          {formData.pastSurgeries.map((surgery, index) => (
+            <div key={index}>
+              <label className='form-label'>
+                Surgery Name:
+                <input className='form-input'
+                  type="text"
+                  name="surgeryName" placeholder='Enter Surgery'
+                  value={surgery.surgeryName || ''}
+                  onChange={(e) => handleInputChange(e, 'pastSurgeries', index)}
+                />
+              </label>
+              <br />
+              <label className='form-label'>
+                Surgery Date:
+                <input className='form-input'
+                  type="date"
+                  name="surgeryDate"
+                  value={surgery.surgeryDate || ''}
+                  onChange={(e) => handleInputChange(e, 'pastSurgeries', index)}
+                />
+              </label>
+              <br />
+            </div>
+          ))}
+          <button type="button" onClick={() => handleAddEntry('pastSurgeries')}>
+            Add Surgery
+          </button>
+        </div>
+
+        <br />
+         
+      </form>
+    </div>
+  );
+}
+
+export default MedicalInfo;
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,10 +1,71 @@
-import React from 'react'
+import React,{useState} from 'react'
+import PersonalInfo from './PersonalInfo';
+import MedicalInfo from './MedicalInfo';
+import FamilyInfo from './FamilyInfo';
+import './Register.css'
+function Register() {
+  const [page,setPage]= useState(0);
+  const [familyMembers,setFamilyMembers]= useState([]);
+  const [formData,setFormData]= useState({
+    username: '',
+    password: '',
+    name: '',
+    age: '',
+    height: '',
+    weight: '',
+    gender: '',
+    bloodGroup: '',
+    dateOfBirth: '',
+    contact: '',
+    address: '',
+    prolongedMedicalConditions: [],
+    allergies: [],
+    pastSurgeries: [],
+    relationship: '', 
+    medicalId: '' ,
+  })
+  
 
-export default function Register() {
+  
+
+  const FormTitles = ["Personal Information","Medical Information","Family Information"];
+
+  const PageDisplay = () =>{
+    if (page === 0){
+      return <PersonalInfo formData={formData} setFormData={setFormData}/>;
+    }else if (page === 1){
+      return <MedicalInfo formData={formData} setFormData={setFormData}/>;
+    }else{
+      return <FamilyInfo familyMembers={familyMembers} setFamilyMembers={setFamilyMembers} />;
+    }
+  }
+
   return (
-    <div>
-      <h1>Register form</h1>
-    </div>
+    <div className='form'>
+      
+        <div className='header'>
+          <h1>{FormTitles[page]}</h1>
+        </div>
+        <div className='body'>{PageDisplay()}</div>
+        <div className='footer'>
+          <button disabled= {page == 0} onClick={() =>{setPage((currPage)=> currPage-1);}}>Prev</button>
+          <button onClick={() =>{
+            if(page ===FormTitles.length-1){
+              alert("FORM SUBMITTED");
+              console.log(formData);
+              console.log(familyMembers);
+            }else{
+              setPage((currPage)=> currPage+1);
+          }
+          }}>
+            { page === FormTitles.length-1 ? "Submit": "Next"}</button>
+        </div>
+
+      </div>
+      
     
   )
 }
+
+export default Register
+
